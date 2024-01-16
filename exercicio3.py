7625cdfe08bcdbeeffdf012b63e5c211eaec6e2c
Lucas Amaral <amaralx360@gmail.com>
2024-01-16 14:49:03 -0300

    exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 14:42:27 2024
+
+@author: amara
+"""
+
+num = int(input('Entre com um numero: '))
+
+if(num>0):
+    print('numero positivo')
+elif(num<0):
+    print('numero negativo')
+else:
+    print('numero nulo')
