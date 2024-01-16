6dff984d456c90d3e2956ee0a08312589aa2f2da
Lucas Amaral <amaralx360@gmail.com>
2024-01-16 14:06:35 -0300

    primeiro commit

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um numero: '))
+
+if(num%2!=0):
+    print("numero impar")
+else:
+    print("numero par")
+
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 13:56:25 2024
+
+@author: amara
+"""
+
